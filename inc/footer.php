232aec4d601591b889eb4976547848c7361b8a8e
Amrendra Kumar <amrendra.kumar@hp.com>
2021-05-22 13:31:05 +0530

    jaysutra e learning
    jaysutra e learning

---
+++ inc/footer.php
@@ -0,0 +1,70 @@
+<div id="footer">
+	
+	<ul>
+	
+		<li>
+			<h2><span>Abo</span>ut Us</h2>
+			<p>This website based on e learing system like nptel online course learing purpose.</p>
+		</li>
+		
+		<li>
+			<h2><span>Con</span>tact Us</h2>
+				<div id="info">
+					<i class="fas fa-map-marker-alt"></i>
+					<p>no:16,xxxxx, xxxxxxss,xxxxx,xxxx-605010.</p>
+				</div>
+				<div id="info" style="margin-bottom:-10px;">
+					<i class="fas fa-phone-square"></i>
+					<p>**********1</p>
+				</div><br clear="all"/>
+				<div id="info">
+					<i class="fas fa-envelope"></i>
+					<p>youremailid@gmail.com</p>
+				</div>
+				<div id="f_share">
+				<div id="fb">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					
+				</div>
+				<div id="gp">
+					<a href="#"><i class="fab fa-google-plus-g"></i></a>
+					
+				</div>
+				<div id="tw">
+					<a href="#"><i class="fab fa-twitter "></i></a>
+					
+				</div>
+				</div>
+			
+		</li>
+		
+		<li>
+			<h2><span>Lea</span>ve Your Massage</h2>
+			
+				<form mehtod="post">
+					
+					<div id="f_input">
+					
+						<i class="fas fa-user"></i>
+						<input type="text" name="query_name" placeholder="Enter Your Name" />
+					</div>
+					
+					<div id="f_input">
+					
+						<i class="fas fa-envelope"></i>
+						<input type="text" name="query_email" placeholder="Enter Your Email" />
+					</div>
+					<textarea name="msg" placeholder="Enter Your Msg"></textarea>
+					
+						<button>Send</button>
+				</form>
+			
+		
+		</li><br clear="all"/>
+	
+	</ul>
+	<h3>copyright 2019 © MCA by Ilavarasi G</h3>
+	
+	
+	
+</div>
---
+++ inc/slider.php
@@ -0,0 +1,7 @@
+<div id="slider">
+
+	<img src="imgs/slider/6.png" />
+
+
+
+</div>
